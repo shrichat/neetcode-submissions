07756e8a335a3cd0b2dd4df5d751d19572b69c6b
shrichat <158341244+shrichat@users.noreply.github.com>
2026-05-15 20:17:47 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        new_s = sorted(s)
+        new_t = sorted(t)
+
+        if new_s == new_t:
+            return True
+
+        else:
+            return False
+
+
+        
