8385d94d03fb5fe28396e6169554fa4d77950624
shrichat <158341244+shrichat@users.noreply.github.com>
2026-05-20 13:30:53 -0500

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ""
+        for word in strs:
+            length = str(len(word))
+            delim = "#"
+            encoded_string+= (length+delim+word)
+
+        return encoded_string
+
+        
+
+
+    def decode(self, s: str) -> List[str]:
+        original_strings = []
+        i = 0
+        while i<len(s):
+            hashtag_index = s.find("#", i)
+            length = int(s[i:hashtag_index])
+
+            start_of_word = hashtag_index + 1
+            end_of_word = start_of_word + length
+            word = s[start_of_word:end_of_word]
+
+            original_strings.append(word)
+
+            i = end_of_word
+
+        return original_strings
+
